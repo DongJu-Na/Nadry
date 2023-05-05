08aed74757f4c0439baa3c0f1d77e147846527d7
ndj <ehdeld123@naver.com>
2023-05-05 17:14:04 +0900

    project init

---
+++ src/main/java/com/nadeul/ndj/NdjApplication.java
@@ -0,0 +1,13 @@
+package com.nadeul.ndj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nadeul/ndj/NdjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadeul.ndj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
