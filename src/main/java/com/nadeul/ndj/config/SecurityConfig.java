5c4f0adf9f4f46d99733e34cc626646ea543bcb2
DongJu-Na <ehdeld123@naver.com>
2023-05-11 14:59:01 +0900

    style : 시큐리티 설정 설명 주석 추가
    시큐리티 설정 설명 주석 추가
    추후 변경 예정

--- src/main/java/com/nadeul/ndj/config/SecurityConfig.java
+++ src/main/java/com/nadeul/ndj/config/SecurityConfig.java
@@ -69,17 +69,20 @@ public class SecurityConfig {
         .frameOptions()
         .sameOrigin()
 
-        // 세션을 사용하지 않기 때문에 STATELESS로 설정
+        // 시큐리티는 기본적으로 세션을 사용
+        // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
         .and()
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
+        
+        // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
         .and()
         .authorizeHttpRequests() // HttpServletRequest 사용 요청 접근제한
         .requestMatchers(WHITE_LIST).permitAll()
         .requestMatchers(PathRequest.toH2Console()).permitAll()
         .anyRequest().authenticated()
-
+        
+        // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
         .and()
         .apply(new JwtSecurityConfig(tokenProvider));
 
