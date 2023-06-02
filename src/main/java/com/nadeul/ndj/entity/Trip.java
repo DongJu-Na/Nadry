27bea86be7504b0d91b100e35c9c06f85dc2135f
ndj <ehdeld123@naver.com>
2023-06-02 23:36:30 +0900

    style : Trip 엔티티 추가
    style : Trip 엔티티 추가

---
+++ src/main/java/com/nadeul/ndj/entity/Trip.java
@@ -0,0 +1,29 @@
+package com.nadeul.ndj.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Trip {
+	
+  @Id
+  @GeneratedValue
+  private Integer tpId;
+  
+  private Integer memId;
+  
+  private Integer posX;
+  private Integer posY;
+  
+  
+  
+}
