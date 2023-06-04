5c1c72f299355bb2c72ff5cd93b5026e0f1ab163
ndj <ehdeld123@naver.com>
2023-06-04 23:32:02 +0900

    style : Comment 엔티티 추가

---
+++ src/main/java/com/nadeul/ndj/entity/Comment.java
@@ -0,0 +1,44 @@
+package com.nadeul.ndj.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Comment {
+	
+  @Id
+  @GeneratedValue
+  private Integer cmId;
+  
+  private Integer bdId;
+  
+  private Integer ptId;
+  
+  private String content;
+  
+  private int like;
+  
+  private String delYn;
+  
+  private Date createDate;
+  
+  private String createBy;
+  
+  private Date updateDate;
+  
+  private String updateBy;
+  
+		
+		
+}
