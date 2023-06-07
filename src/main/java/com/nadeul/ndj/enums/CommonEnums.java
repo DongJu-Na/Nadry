fbf5d105b8abe3bbdfb5d770ab86e02264931a56
DongJu-Na <ehdeld123@naver.com>
2023-06-07 11:47:55 +0900

    style : 공통 응답코드 , 응답메세지 Enum
    init

---
+++ src/main/java/com/nadeul/ndj/enums/CommonEnums.java
@@ -0,0 +1,13 @@
+package com.nadeul.ndj.enums;
+
+/**
+  * @FileName : CommonEnums.java
+  * @Project : ndj
+  * @Date : 2023. 6. 7. 
+  * @작성자 : ndj
+  * @변경이력 :
+  * @프로그램 설명 : 공통 응답코드&응답메세지 Enum
+  */
+public enum CommonEnums {
+
+}
