6de251ebbe8b255fa9e0af71f9a92d4808444587
DongJu-Na <ehdeld123@naver.com>
2023-06-09 12:13:17 +0900

    style : pointController init

---
+++ src/main/java/com/nadeul/ndj/controller/PointController.java
@@ -0,0 +1,22 @@
+package com.nadeul.ndj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/point")
+public class PointController {
+	private static final int EARTH_RADIUS = 6371; // 지구의 반지름 (단위: km)
+	// 위도 경도 계산
+  public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    double dLat = Math.toRadians(lat2 - lat1);
+    double dLon = Math.toRadians(lon2 - lon1);
+    double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    double distance = EARTH_RADIUS * c;
+    return distance;
+  }
+  
+}
