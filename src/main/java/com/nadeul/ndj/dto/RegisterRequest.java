d78d7407de0ff71a570aea9b279dc49a27cf1245
DongJu-Na <ehdeld123@naver.com>
2023-06-12 15:58:23 +0900

    style : 회원가입 dto 수정
    스웨거 주석 추가

--- src/main/java/com/nadeul/ndj/dto/RegisterRequest.java
+++ src/main/java/com/nadeul/ndj/dto/RegisterRequest.java
@@ -1,6 +1,8 @@
 package com.nadeul.ndj.dto;
 
 import com.nadeul.ndj.model.Role;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,9 +22,15 @@ ROLE VARCHAR NOT NULL 나드리 이용 권한
 LAST_LOGIN_DATE DATE 최종 로그인 일자
 REGISTER_DATE DATE 가입일자
 */
+	
+	@Schema(description = "이름" , example = "나동주")
   private String name;
+	@Schema(description = "이메일" , example = "ehdeld123@gmail.com")
   private String email;
+	@Schema(description = "비밀번호" , example = "**********")
   private String password;
+	@Schema(description = "나드리이용권한" , example = "")
   private Role role;
+	@Schema(description = "생일 yyyy-mm-dd" , example = "1995-08-22")
   private String birthDay;
 }
