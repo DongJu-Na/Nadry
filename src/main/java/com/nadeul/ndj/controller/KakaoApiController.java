6252c7f87983fb6189b9351a164f1394609fdeea
DongJu-Na <ehdeld123@naver.com>
2023-06-12 18:30:12 +0900

    style : 카카오 컨트롤러 수정
    불필요 코드 수정

--- src/main/java/com/nadeul/ndj/controller/KakaoApiController.java
+++ src/main/java/com/nadeul/ndj/controller/KakaoApiController.java
@@ -1,14 +1,7 @@
 package com.nadeul.ndj.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.nadeul.ndj.dto.AuthenticationRequest;
-import com.nadeul.ndj.dto.AuthenticationResponse;
-import com.nadeul.ndj.service.AuthenticationService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class KakaoApiController {
 	
-	private final AuthenticationService service;
-		// 임시 인증/인가 로직 필요
-	  @PostMapping("/authenticate")
-	  public ResponseEntity<AuthenticationResponse> authenticate( @RequestBody AuthenticationRequest request ) {
-	    return ResponseEntity.ok(service.authenticate(request));
-	  }
+
 
 }
