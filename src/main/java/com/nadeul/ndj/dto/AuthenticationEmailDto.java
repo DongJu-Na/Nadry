1c84f9e14518895cadb4ee070a42b6051dc2df14
ndj <ehdeld123@naver.com>
2023-06-20 22:18:43 +0900

    style : AuthenticationEmailDto 추가
    style : AuthenticationEmailDto 추가

---
+++ src/main/java/com/nadeul/ndj/dto/AuthenticationEmailDto.java
@@ -0,0 +1,15 @@
+package com.nadeul.ndj.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationEmailDto {
+
+  private String email;
+}
