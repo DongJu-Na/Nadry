3c71d67d3035115b30cf2772ef3df2b634b6d95c
ndj <ehdeld123@naver.com>
2023-06-21 23:49:40 +0900

    feat : 포인트 적립 로직 init
    임시

--- src/main/java/com/nadeul/ndj/controller/PointController.java
+++ src/main/java/com/nadeul/ndj/controller/PointController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v2/point")
 public class PointController {
-	private static final int EARTH_RADIUS = 6371; // 지구의 반지름 (단위: km)
-	// 위도 경도 계산
+  private static final int EARTH_RADIUS = 6371; // 지구의 반지름 (단위: km)
+  
+  // 위도 경도 계산
   public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
     double dLat = Math.toRadians(lat2 - lat1);
     double dLon = Math.toRadians(lon2 - lon1);
