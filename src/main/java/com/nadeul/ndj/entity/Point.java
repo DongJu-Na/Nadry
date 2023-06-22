51a140527e71743183d31d0747b93932e23ab38e
DongJu-Na <ehdeld123@naver.com>
2023-06-22 16:34:52 +0900

    style : 포인트 엔티티 수정
    관계 설정 추가

--- src/main/java/com/nadeul/ndj/entity/Point.java
+++ src/main/java/com/nadeul/ndj/entity/Point.java
@@ -3,6 +3,8 @@ package com.nadeul.ndj.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,12 +21,17 @@ public class Point {
   @GeneratedValue
   private Integer poId;
   
-  private Integer memId;
-  
   private Integer point;
-  
   private String blackYn;
   
-		
+  @OneToOne
+  @JoinColumn(name = "memId")
+  private Member member;
+  
+  public Point update(Integer point, String blackYn) {
+    this.point = point;
+    this.blackYn = blackYn;
+    return this;
+  }
 		
 }
