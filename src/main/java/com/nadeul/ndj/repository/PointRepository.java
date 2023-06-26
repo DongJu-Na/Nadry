6994d1f535342033e9702af2db78884a684ab36e
DongJu-Na <ehdeld123@naver.com>
2023-06-26 18:13:12 +0900

    style : PointRepository 수정
    회원번호로 포인트 찾기 추가

--- src/main/java/com/nadeul/ndj/repository/PointRepository.java
+++ src/main/java/com/nadeul/ndj/repository/PointRepository.java
@@ -1,9 +1,12 @@
 package com.nadeul.ndj.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.nadeul.ndj.entity.Member;
 import com.nadeul.ndj.entity.Point;
 
 public interface PointRepository extends JpaRepository<Point, Integer> {
-	
+	Optional<Point> findByPoint(Member memId);
 }
