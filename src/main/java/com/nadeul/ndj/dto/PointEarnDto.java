29314f75bdd94fc59894efa3aff05e10f92ceef9
DongJu-Na <ehdeld123@naver.com>
2023-06-26 18:12:33 +0900

    style : PointEarnDto 수정
    추후 클라이언트에서 주는 데이터 확인 이후 수정이 생길 수 있음

--- src/main/java/com/nadeul/ndj/dto/PointEarnDto.java
+++ src/main/java/com/nadeul/ndj/dto/PointEarnDto.java
@@ -1,6 +1,9 @@
 package com.nadeul.ndj.dto;
 
 
+import java.util.Date;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,11 +13,30 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(description = "포인트 적립 DTO")
 public class PointEarnDto {
 	/** exif.js 메타데이터 값들 보고 엔티티 , 컬럼값들도 변경 필요함   
 	* 
 	**/
+	
+	@Schema(description = "컨텐츠ID")
+	private String contentId;
+	
+	@Schema(description = "tourAPI 위도")
 	private Float posX;
+	
+	@Schema(description = "tourAPI 경도")
 	private Float posY;
+	
+	@Schema(description = "실제 위도")
+	private Float realPosX;
+	
+	@Schema(description = "실제 경도")
+	private Float realPosY;
+	
+	@Schema(description = "여행 일자 yyyy-mm-dd")
+	
+	private Date tripDate;
+	
 	
 }
