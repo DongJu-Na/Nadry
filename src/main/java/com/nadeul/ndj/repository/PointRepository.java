94599a000b68b485b3f9dcf99459b8ec52a36858
DongJu-Na <ehdeld123@naver.com>
2023-06-27 16:21:33 +0900

    refact : 포인트 회원 조회 쿼리 수정

--- src/main/java/com/nadeul/ndj/repository/PointRepository.java
+++ src/main/java/com/nadeul/ndj/repository/PointRepository.java
@@ -4,9 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.nadeul.ndj.entity.Member;
 import com.nadeul.ndj.entity.Point;
 
 public interface PointRepository extends JpaRepository<Point, Integer> {
-	Optional<Point> findByPoint(Member memId);
+	
+	Optional<Point> findByMemberMemId(Integer memId);
 }
