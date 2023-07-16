2925bc48892b27b401e220c9b29b6b5da5fb6cb6
ndj <ehdeld123@naver.com>
2023-07-16 22:11:55 +0900

    style : BoardController 불필요 코드 주석

--- src/main/java/com/nadeul/ndj/controller/BoardController.java
+++ src/main/java/com/nadeul/ndj/controller/BoardController.java
@@ -3,12 +3,18 @@ package com.nadeul.ndj.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.nadeul.ndj.dto.ApiResponse;
 import com.nadeul.ndj.entity.Board;
+import com.nadeul.ndj.enums.ApiResponseEnum;
 import com.nadeul.ndj.service.BoardService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,6 +32,29 @@ public class BoardController<T> {
     public ResponseEntity<ApiResponse<List<Board>>> list() {
         return ResponseEntity.ok(boardService.list());
     }
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<Board>> getBoardById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(boardService.getBoardById(id));
+    }
+	
+	@PostMapping("/")
+    public ResponseEntity<ApiResponse<Board>> createBoard(@RequestBody Board board) {
+        return ResponseEntity.ok(boardService.createBoard(board));
+    }
+	
+	@PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<Board>> updateBoard(@PathVariable("id") Integer id, @RequestBody Board board) {
+        return ResponseEntity.ok(boardService.updateBoard(id, board));
+    }
+	/*
+	@DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> deleteBoard(@PathVariable("id") Integer id) {
+        boardService.deleteBoard(id);
+        return ResponseEntity.ok(ApiResponse.successResponse(ApiResponseEnum.SUCCESS, null, null, null));
+    }
+    */
+    
 	
 
     
