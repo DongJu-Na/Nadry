fec3b21629c9b438f3c621252e9992e486d562b4
ndj <ehdeld123@naver.com>
2023-07-23 20:49:32 +0900

    style : MemberDto 주석 추가

--- src/main/java/com/nadeul/ndj/dto/MemberDto.java
+++ src/main/java/com/nadeul/ndj/dto/MemberDto.java
@@ -2,6 +2,7 @@ package com.nadeul.ndj.dto;
 
 import com.nadeul.ndj.model.Role;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,12 +13,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class MemberDto {
-	
+  
+  @Schema(description = "인덱스" , example = "1")
   private Integer id;
+  @Schema(description = "이름" , example = "나동주")
   private String name;
+  @Schema(description = "이메일" , example = "ehdeld123@gmail.com")
   private String email;
+  @Schema(description = "비밀번호" , example = "***********")
   private String password;
+  @Schema(description = "생일" , example = "19950822")
   private String birthDay;
+  @Schema(description = "역할" , example = "USER")
   private Role role;
   
   
