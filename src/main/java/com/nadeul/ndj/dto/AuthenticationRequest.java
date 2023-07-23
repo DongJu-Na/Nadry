f4168e01e97e5d7c798bdea77fad6d49dd679d37
ndj <ehdeld123@naver.com>
2023-07-23 20:19:43 +0900

    style : AuthenticationRequest DTO 주석 추가

--- src/main/java/com/nadeul/ndj/dto/AuthenticationRequest.java
+++ src/main/java/com/nadeul/ndj/dto/AuthenticationRequest.java
@@ -1,5 +1,6 @@
 package com.nadeul.ndj.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
-
+	
+  @Schema(description = "이메일" , example = "ehdeld123@gmail.com")
   private String email;
+  
+  @Schema(description = "비밀번호" , example = "************")
   String password;
 }
