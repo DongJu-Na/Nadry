34cc160542bc60f89835c9dd3a4558c509df7704
ndj <ehdeld123@naver.com>
2023-07-23 20:19:58 +0900

    style : AuthenticationResponse 주석 추가

--- src/main/java/com/nadeul/ndj/dto/AuthenticationResponse.java
+++ src/main/java/com/nadeul/ndj/dto/AuthenticationResponse.java
@@ -1,6 +1,8 @@
 package com.nadeul.ndj.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,9 +13,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationResponse {
-
+  
+  @Schema(description = "액세스 토큰" , example = "")
   @JsonProperty("access_token")
   private String accessToken;
+  
+  @Schema(description = "리프레시 토큰" , example = "")
   @JsonProperty("refresh_token")
   private String refreshToken;
 }
