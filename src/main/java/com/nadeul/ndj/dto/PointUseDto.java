49f6a2e063be3c1f4584523225e145f9d46970c9
ndj <ehdeld123@naver.com>
2023-07-24 23:44:37 +0900

    style : PointUseDto 주석 추가

--- src/main/java/com/nadeul/ndj/dto/PointUseDto.java
+++ src/main/java/com/nadeul/ndj/dto/PointUseDto.java
@@ -1,6 +1,7 @@
 package com.nadeul.ndj.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,7 +15,9 @@ public class PointUseDto {
 	/** exif.js 메타데이터 값들 보고 엔티티 , 컬럼값들도 변경 필요함   
 	* 
 	**/
+	@Schema(description = "tourAPI 위도")
 	private Float posX;
+	@Schema(description = "tourAPI 경도")
 	private Float posY;
 	
 }
