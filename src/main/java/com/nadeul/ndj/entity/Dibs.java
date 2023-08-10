b9497814be60528b9fcbdc4f14a6299a63911586
ndj <ehdeld123@naver.com>
2023-08-10 22:03:16 +0900

    style : 찜 테이블 추가

---
+++ src/main/java/com/nadeul/ndj/entity/Dibs.java
@@ -0,0 +1,40 @@
+package com.nadeul.ndj.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Dibs {
+	
+  @Id
+  @GeneratedValue
+  private Integer diId;
+  
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "memId", insertable = true, updatable = true)
+  public Member member;
+  
+  private Date dibsDate;
+  
+  private String contentId;
+  
+  private String contentName;
+  
+  private String contentImageUrl;
+  
+  
+}
