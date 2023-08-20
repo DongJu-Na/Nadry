5e2a1b73ad34cd84a5eebbb3d9241ea1ce07a513
ndj <ehdeld123@naver.com>
2023-08-20 19:25:53 +0900

    style : product 레포지토리 추가

---
+++ src/main/java/com/nadeul/ndj/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.nadeul.ndj.repository;
+
+import com.nadeul.ndj.entity.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
