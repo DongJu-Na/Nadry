db2691211ba2108a03ba9f9b015b3b53c7042899
ndj <ehdeld123@naver.com>
2023-08-21 00:03:11 +0900

    style : 상품 repo 추가

--- src/main/java/com/nadeul/ndj/repository/ProductRepository.java
+++ src/main/java/com/nadeul/ndj/repository/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.nadeul.ndj.repository;
 
-import com.nadeul.ndj.entity.Product;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+import com.nadeul.ndj.entity.Product;
 
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+	
+  Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
+  
 }
