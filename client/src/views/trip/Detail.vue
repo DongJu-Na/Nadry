dfc40bc4d9219240b29bd2536740811ab52b39e2
buzzrstudio <keepsoul@gmail.com>
2023-08-23 00:05:48 +0900

    edit: 여행 상세 페이지 수정

---
+++ client/src/views/trip/Detail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- primary image -->
+    <img src="https:/placehold.it/900x900" alt="" />
+    <!-- 찜하기 / 공유하기 / 이미지더보기 -->
+    <!-- tag -->
+    <!-- title -->
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
