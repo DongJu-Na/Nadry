dc866e53ff704fbf80fc1442956f3b6a14ddcbc5
buzzrstudio <keepsoul@gmail.com>
2023-08-22 23:55:43 +0900

    add: logout 메뉴 추가

--- client/src/views/Menu.vue
+++ client/src/views/Menu.vue
@@ -3,6 +3,7 @@
     <nav class="flex flex-col gap-3">
       <router-link to="/login" class="p-3 bg-zinc-50">로그인</router-link>
       <router-link to="/join" class="p-3 bg-zinc-50">회원가입</router-link>
+      <router-link to="/logout" class="p-3 bg-zinc-50">로그아웃</router-link>
     </nav>
   </div>
 </template>
