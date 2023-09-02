e40bf9f082864cbfbc3d18eae93943b22c50d4ca
buzzrstudio <keepsoul@gmail.com>
2023-09-03 00:33:19 +0900

    edit: lottie 적용

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,9 @@ import piniaPersist from 'pinia-plugin-persist';
 import pinia from '@/store';
 import { router } from '@/routes';
 import App from './App.vue';
+import Vue3Lottie from 'vue3-lottie';
 import './style.css';
 
 pinia.use(piniaPersist);
 
-createApp(App).use(pinia).use(router).mount('#app');
+createApp(App).use(pinia).use(router).use(Vue3Lottie).mount('#app');
