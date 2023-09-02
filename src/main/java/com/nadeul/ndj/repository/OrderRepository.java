1e07ffd479229045398da177c7aa11d9143c2d45
ndj <ehdeld123@naver.com>
2023-09-02 21:58:32 +0900

    style : Orders Repo 이름 수정

--- src/main/java/com/nadeul/ndj/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nadeul.ndj.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nadeul.ndj.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-	
-}
