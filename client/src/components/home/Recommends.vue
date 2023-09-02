2de2f527f6fc370a8e4a100417780d94c640e62a
buzzrstudio <keepsoul@gmail.com>
2023-09-03 00:37:38 +0900

    edit: 여행 카테고리 아이콘 순서 변경

--- client/src/components/home/Recommends.vue
+++ client/src/components/home/Recommends.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="px-5 my-20">
-    <h2 class="text-lg font-semibold">추천 여행지</h2>
+  <div class="px-5 my-10">
+    <h2 class="text-lg font-semibold">어떤 여행을 찾으세요?</h2>
     <ul class="grid grid-cols-4 gap-4 mt-5">
       <li v-for="item in categories" :key="item.id">
         <a href="" class="flex flex-col items-center">
           <img :src="`/svg/recommend_${item.icon}.svg`" />
-          <span class="text-sm font-medium text-zinc-800 mt-2">{{ item.name }}</span>
+          <span class="mt-2 text-sm font-medium text-zinc-800">{{ item.name }}</span>
         </a>
       </li>
     </ul>
@@ -17,44 +17,44 @@ import { ref } from 'vue';
 
 const categories = ref([
   {
-    id: 1,
+    id: 12,
     name: '관광지',
     icon: 'tower',
   },
   {
-    id: 2,
+    id: 14,
     name: '문화시설',
     icon: 'library',
   },
   {
-    id: 3,
-    name: '축제공연',
+    id: 15,
+    name: '축제/공연',
     icon: 'festival',
   },
   {
-    id: 4,
+    id: 25,
+    name: '여행코스',
+    icon: 'trail',
+  },
+  {
+    id: 28,
     name: '레포츠',
     icon: 'reports',
   },
   {
-    id: 5,
+    id: 32,
     name: '숙박',
     icon: 'bed',
   },
   {
-    id: 6,
+    id: 38,
     name: '쇼핑',
     icon: 'shop',
   },
   {
-    id: 7,
+    id: 39,
     name: '음식점',
     icon: 'rice',
-  },
-  {
-    id: 8,
-    name: '여행코스',
-    icon: 'trail',
   },
 ]);
 </script>
