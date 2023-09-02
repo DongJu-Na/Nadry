1ee80afbc3cb56b1bb1db6ca9c115f3465f086a0
buzzrstudio <keepsoul@gmail.com>
2023-09-03 00:35:59 +0900

    add: 여행 상세 추가

--- client/src/views/trip/Detail.vue
+++ client/src/views/trip/Detail.vue
@@ -1,37 +1,31 @@
 <template>
-  <div>
+  <div v-if="detailCommon && detailImage" class="">
     <!-- primary image -->
-    <!-- <img src="https:/placehold.it/900x900" alt="" /> -->
+    <img :src="detailImage[0].originimgurl" alt="" />
+    {{ detailIntro }}
     <div class="p-5">
       <!-- extra -->
-      <div class="flex">
-        <!-- like -->
+      <!-- <div class="flex">
         <div>
           <img src="" alt="" />
           <span>찜하기</span>
         </div>
-        <!-- share -->
         <div>
           <img src="" alt="" />
           <span>공유하기</span>
         </div>
-        <!-- images -->
-        <div>
-          <img src="" alt="" />
-        </div>
-      </div>
+      </div> -->
       <!-- tag -->
-      <div class="flex text-emerald-500">
+      <!-- <div class="flex text-emerald-500">
         <span>#경주</span>
         <span>#유적지</span>
         <span>#핫플</span>
-      </div>
+      </div> -->
       <!-- title -->
-      <h1>동궁과 월지(안압지)</h1>
-      <dl>
-        <dt>경상북도 경주시 원화로 102 (인왕동)</dt>
-        <dd>http://www.gyeongju.go.kr/tour</dd>
-      </dl>
+      <h1>{{ detailCommon.title }}</h1>
+      <div>{{ detailCommon.add1 }}</div>
+      <div>{{ detailCommon.add2 }}</div>
+      <div v-html="detailCommon.homepage"></div>
       <!-- rating -->
       <div>
         <div>4.9</div>
@@ -39,32 +33,138 @@
       </div>
       <div>387개의 리뷰가 있습니다</div>
       <!-- description -->
-      <div>
-        <p>
-          경주 동궁과 월지는 신라 왕궁의 별궁터이다. 다른 부속건물들과 함께 왕자가 거처하는 동궁으로
-          사용되면서, 나라의 경사가 있을 때나 귀한 손님을 맞을 때 이곳에서 연회를 베풀었다고 한다.
-          신라 경순왕이 견훤의 침입을 받은 뒤, 931년에 고려 태조 왕건을 초청하여 위급한 상황을
-          호소하며 잔치를 베풀었던 곳이기도 하다. 신라는 삼국을 통일한 후 문무왕 14년(674)에 큰
-          연못을 파고 못 가운데에 3개의 섬과 못의 북동쪽으로 12봉우리의 산을 만들었으며, 여기에
-          아름다운 꽃과 나무를 심고 진귀한 새와 짐승을 길렀다고 전해진다. 고려시대의
-          『삼국사기』에는 임해전에 대한 기록만 나오고 안압지에 대해서는 아무런 언급이 없으며, 이 후
-          신라가 멸망하고 고려와 조선에 이르러 이곳이 폐허가 되자, 시인 묵객들이 연못을 보며
-          "화려했던 궁궐은 간데없고 기러기와 오리만 날아든다."는 쓸쓸한 시 구절을 읊조리며, 이 곳을
-          기러기 '안'자와 오리'압'자를 써서 '안압지'라고 불렀다고 한다.
-        </p>
-      </div>
+      <div v-html="detailCommon.overview" class="mt-10 break-keep"></div>
       <!-- etc information -->
-      <div>
+      <div class="mt-10">
         <h3>이용안내</h3>
       </div>
       <!-- reviews -->
-      <div>
+      <div class="mt-10">
         <h3>리뷰</h3>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted, computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import {
+  getTripDetailIntro,
+  getTripDetailInfo,
+  getTripDetailImage,
+  getTripDetailCommon,
+} from '@/api';
+import { useMainStore } from '@/store';
 
-<style lang="scss" scoped></style>
+const store = useMainStore();
+
+const route = useRoute();
+const router = useRouter();
+
+const detailIntro = ref(null);
+const detailInfo = ref(null);
+const detailImage = ref(null);
+const detailCommon = ref(null);
+
+// const contentId = computed(() => {
+//   return route.params.id;
+// });
+
+const fetchTripData = async () => {
+  // 로딩중 출력
+  store.state.setLoading(true);
+  // Intro
+  try {
+    const payload = {
+      contentId: route.params.id,
+      contentTypeId: route.params.type,
+    };
+    const {
+      status,
+      data: {
+        response: {
+          body: {
+            items: { item },
+          },
+        },
+      },
+    } = await getTripDetailIntro(payload);
+    console.log(status, item[0]);
+    if (status === 200 && item[0]) {
+      detailIntro.value = item[0];
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  // Info
+
+  // Image
+  try {
+    const payload = {
+      contentId: route.params.id,
+      imageYN: 'Y',
+      subImageYN: 'Y',
+    };
+    const {
+      status,
+      data: {
+        response: {
+          body: {
+            items: { item },
+          },
+        },
+      },
+    } = await getTripDetailImage(payload);
+    console.log(status, item);
+    if (status === 200 && item) {
+      detailImage.value = item;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  // Common
+  try {
+    const payload = {
+      contentId: route.params.id,
+      contentTypeId: route.params.type,
+      defaultYN: 'Y',
+      firstImageYN: 'Y',
+      areacodeYN: 'Y',
+      catcodeYN: 'Y',
+      addrinfoYN: 'Y',
+      mapinfoYN: 'Y',
+      overviewYN: 'Y',
+    };
+    const {
+      status,
+      data: {
+        response: {
+          body: {
+            items: { item },
+          },
+        },
+      },
+    } = await getTripDetailCommon(payload);
+    console.log(status, item[0]);
+    if (status === 200 && item[0]) {
+      detailCommon.value = item[0];
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  // 로딩중 출력
+  store.state.setLoading(false);
+};
+
+onMounted(async () => {
+  await router.isReady();
+
+  fetchTripData();
+
+  // console.log(contentId.value);
+});
+</script>
