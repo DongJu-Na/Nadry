cc92222244a33a639cd3e13e19efcdb5f71c69f8
buzzrstudio <keepsoul@gmail.com>
2023-09-03 00:33:33 +0900

    edit: 여행지 조회 관련 api 추가

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -33,3 +33,25 @@ export const join = (payload) => {
 export const getProducts = () => {
   return instanceWithNoAuth.get('/api/v1/product/list');
 };
+
+// 여행지 검색
+export const searchTrip = (payload) => {
+  return instanceWithNoAuth.post('/api/v1/tour/searchKeyword1/Kor', payload);
+};
+
+// 여행지 상세 조회 - Intro
+export const getTripDetailIntro = (payload) => {
+  return instanceWithNoAuth.post('/api/v1/tour/detailIntro1/Kor', payload);
+};
+// 여행지 상세 조회 - Info
+export const getTripDetailInfo = (payload) => {
+  return instanceWithNoAuth.post('/api/v1/tour/detailInfo1/Kor', payload);
+};
+// 여행지 상세 조회 - Image
+export const getTripDetailImage = (payload) => {
+  return instanceWithNoAuth.post('/api/v1/tour/detailImage1/Kor', payload);
+};
+// 여행지 상세 조회 - Common
+export const getTripDetailCommon = (payload) => {
+  return instanceWithNoAuth.post('/api/v1/tour/detailCommon1/Kor', payload);
+};
