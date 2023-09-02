9c10b9a5bd90933bcd6ebb5da888097abdb2c7ba
buzzrstudio <keepsoul@gmail.com>
2023-09-03 00:32:33 +0900

    add: 404

---
+++ client/src/views/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center justify-center flex-1 grow-1">
+    <img src="/img/404.png" class="w-[50%]" />
+    <span class="text-lg text-zinc-400">페이지가 없습니다</span>
+    <BackButton />
+  </div>
+</template>
+
+<script setup>
+import BackButton from '@/components/common/atoms/BackButton.vue';
+</script>
