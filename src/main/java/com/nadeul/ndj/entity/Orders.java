c26b18c0dd0a0c434623609efab02ce46d828b0b
ndj <ehdeld123@naver.com>
2023-09-02 21:51:49 +0900

    Style : Order 엔티티 수정

--- src/main/java/com/nadeul/ndj/entity/Orders.java
+++ src/main/java/com/nadeul/ndj/entity/Orders.java
@@ -5,8 +5,11 @@ import com.nadeul.ndj.enums.OrderStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -20,7 +23,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table
-public class Order {
+public class Orders {
 
 	@Id
 	@GeneratedValue
@@ -29,10 +32,21 @@ public class Order {
     @NotNull
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
-    /*
+    
     @NotNull
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ctId", insertable = true, updatable = true)
     private Cart cart;
+    
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memId", insertable = true, updatable = true)
+    private Member member;
+    
+    
+    
+    /*
+  
 
     @NotNull
     @OneToOne(fetch = LAZY)
