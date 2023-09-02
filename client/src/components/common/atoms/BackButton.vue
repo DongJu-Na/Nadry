b9354760cb29146cef5fc6f0125f250fc57939ae
buzzrstudio <keepsoul@gmail.com>
2023-09-03 00:36:40 +0900

    add: 뒤로가기 컴포넌트

---
+++ client/src/components/common/atoms/BackButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <a @click="router.back()" class="mt-5 button">
+    <i class="mr-2 text-xl las la-arrow-left"></i>
+    <span>돌아가기</span>
+  </a>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
