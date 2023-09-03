dd65e403169b7bb0d62c7af85fdd7995a5aa79f7
ndj <ehdeld123@naver.com>
2023-09-03 23:23:06 +0900

    style : Review 엔티티 수정
    Date - > LocalDateTime

--- src/main/java/com/nadeul/ndj/entity/Review.java
+++ src/main/java/com/nadeul/ndj/entity/Review.java
@@ -1,12 +1,14 @@
 package com.nadeul.ndj.entity;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,11 +36,11 @@ public class Review {
   
   private String delYn;
   
-  private Date createDate;
+  private LocalDateTime createDate;
   
   private String createBy;
   
-  private Date updateDate;
+  private LocalDateTime updateDate;
   
   private String updateBy;
   
@@ -49,5 +51,9 @@ public class Review {
   @OneToMany
   @JoinColumn(name = "rgId")
   private List<ReviewGrade> reviewGrade;
+  
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "memId", insertable = true, updatable = true)
+  private Member member;
 		
 }
