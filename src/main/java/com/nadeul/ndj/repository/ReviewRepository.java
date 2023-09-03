bcc92c8fed6715d8d0a8b6a0ff3debeab3d10036
ndj <ehdeld123@naver.com>
2023-09-03 21:37:04 +0900

    style : ReviewRepo 추가

---
+++ src/main/java/com/nadeul/ndj/repository/ReviewRepository.java
@@ -0,0 +1,17 @@
+package com.nadeul.ndj.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.nadeul.ndj.entity.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+	 Optional<List<Review>> findByContentId(Integer contentId);
+	 
+	 @Query("SELECT AVG(rg.rating) FROM Review r JOIN r.reviewGrade rg WHERE r.contentId = :contentId")
+	 Optional<Double> findAverageRatingByContentId(@Param("contentId") String contentId);
+}
