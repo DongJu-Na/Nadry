d12434c05b050fffcb3ab91b73eaa9f86b98277f
ndj <ehdeld123@naver.com>
2023-09-05 00:11:43 +0900

    feat : security whiteList 추가

--- src/main/java/com/nadeul/ndj/config/SecurityConfig.java
+++ src/main/java/com/nadeul/ndj/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
   
   public static final String[] whiteListedRoutes = new String[]{
       "/api/v1/**",
+      "/api/v2/*/list",
       "/v2/api-docs",
       "/v3/api-docs",
       "/v3/api-docs/**",
