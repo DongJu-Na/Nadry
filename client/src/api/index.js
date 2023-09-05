d86c237fde62de32fc361b3283ce76a82697c068
buzzrstudio <keepsoul@gmail.com>
2023-09-06 08:34:13 +0900

    edit: api 정리

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -25,7 +25,7 @@ const makeDynamicUrl = (params) => {
     queryString = `?${queryString}`;
   }
   return queryString;
-}
+};
 
 const instance = createInstance();
 const instanceWithNoAuth = createInstanceWithNoAuth();
@@ -40,7 +40,7 @@ export const join = (payload) => {
   return instanceWithNoAuth.post('/api/v1/auth/register', payload);
 };
 
-// 상품 조회 - market products 
+// 상품 조회 - market products
 // param : { "page": 1, "size": 10, "sort": ["name"]} 그냥 객체로 전송 시 모든 데이터 조회
 export const getProducts = () => {
   return instanceWithNoAuth.get('/api/v2/product/list');
@@ -77,17 +77,17 @@ export const getTripReview = (payload) => {
 // 여행리뷰 등록
 // param : {"rvId" : "" , "contentId": "2894451", "content": "먹태깡 재고가 많아서 좋아요.", "reviewRating": "4.5" , "reviewImage" : file}  rvId 는 수정 시 필수 등록 시에는 사용 안하는 값 reviewImage는 파일로 전송주시면 됩니다(사진있을시)
 export const postTripReview = (payload) => {
-  return instanceWithNoAuth.post('/api/v2/review/' , payload);
+  return instanceWithNoAuth.post('/api/v2/review/', payload);
 };
 
 // 여행리뷰 수정
 // param : {"rvId" : "" , "contentId": "2894451", "content": "마음이 변했어요 별로에요.", "reviewRating": "1.0" , "reviewImage" : file}  rvId 는 수정 시에는 필수값
 export const putTripReview = (payload) => {
-  return instanceWithNoAuth.put('/api/v2/review/' , payload);
+  return instanceWithNoAuth.put('/api/v2/review/', payload);
 };
 
 // 여행리뷰 좋아요
 // param : {"rvId" : "" , "rlId": "", "contentId": ""}   rvId 리뷰번호 필수  rlId 리뷰좋아요 번호 삭제 시 필수
 export const postTripLike = (payload) => {
-  return instanceWithNoAuth.post('/api/v2/review/reviewLike' , payload);
+  return instanceWithNoAuth.post('/api/v2/review/reviewLike', payload);
 };
