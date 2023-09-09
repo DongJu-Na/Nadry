1bed9787c40cfd91488b6722a33e2f0b93dec00d
ndj <ehdeld123@naver.com>
2023-09-10 00:01:14 +0900

    refact : 카카오로그인 기능 수정

--- src/main/java/com/nadeul/ndj/oauth2/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/nadeul/ndj/oauth2/OAuth2LoginSuccessHandler.java
@@ -1,7 +1,8 @@
 package com.nadeul.ndj.oauth2;
 
-
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Optional;
 
@@ -12,8 +13,10 @@ import org.springframework.stereotype.Component;
 
 import com.nadeul.ndj.dto.MemberDto;
 import com.nadeul.ndj.entity.Member;
+import com.nadeul.ndj.entity.Token;
+import com.nadeul.ndj.model.TokenType;
 import com.nadeul.ndj.repository.MemberRepository;
-import com.nadeul.ndj.service.AuthenticationService;
+import com.nadeul.ndj.repository.TokenRepository;
 import com.nadeul.ndj.service.JwtService;
 
 import jakarta.servlet.ServletException;
@@ -22,14 +25,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final MemberRepository userRepository;
-//    private final AuthenticationService authenticationService;
-//    private final JwtService jwtService;
+    private final TokenRepository tokenRepository;
+    private final JwtService jwtService;
     
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -45,25 +51,45 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         		
         		Optional<Member> existingMember = userRepository.findByEmail(email);
 
-            // User의 Role이 GUEST일 경우 처음 요청한 회원이므로 회원가입 페이지로 리다이렉트 ((Member) oAuth2User).getRole() == Role.USER
             if(!existingMember.isPresent()) {
                 response.sendRedirect("https://www.nadry.shop/login"); // 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
             } else {
-            	/*
             	String jwtToken = jwtService.generateToken(existingMember.get());
-                String refreshToken = jwtService.generateRefreshToken(existingMember.get());
-                authenticationService.revokeAllUserTokens(existingMember.get());
-                authenticationService.saveUserToken(existingMember.get(), jwtToken);
-                
-            	MemberDto memberDto = new MemberDto();
-			  			  memberDto.setId(existingMember.get().getMemId());
-			  			  memberDto.setName(existingMember.get().getName());
-			  			  memberDto.setEmail(existingMember.get().getEmail());
-			  			  memberDto.setPassword(existingMember.get().getPassword());
-			  			  memberDto.setBirthDay(existingMember.get().getBirthDay());
-			  			  memberDto.setRole(existingMember.get().getRole());
-			  			  memberDto.setProfileUrl(existingMember.get().getProfileUrl());
-			*/  			  
+            	String refreshToken = jwtService.generateRefreshToken(existingMember.get());
+            	
+            	var validUserTokens = tokenRepository.findAllValidTokenByUser(existingMember.get().getMemId());
+            	if (validUserTokens.isEmpty())
+            	      return;
+            	    validUserTokens.forEach(token -> {
+            	      token.setExpired(true);
+            	      token.setRevoked(true);
+            	    });
+            	    tokenRepository.saveAll(validUserTokens);
+        	    var token = Token.builder()
+        	            .member(existingMember.get())
+        	            .token(jwtToken)
+        	            .tokenType(TokenType.BEARER)
+        	            .expired(false)
+        	            .revoked(false)
+        	            .build();
+        	        tokenRepository.save(token);   
+        	        
+    	          MemberDto memberDto = new MemberDto();
+	  			  memberDto.setId(existingMember.get().getMemId());
+	  			  memberDto.setName(existingMember.get().getName());
+	  			  memberDto.setEmail(existingMember.get().getEmail());
+	  			  memberDto.setPassword(existingMember.get().getPassword());
+	  			  memberDto.setBirthDay(existingMember.get().getBirthDay());
+	  			  memberDto.setRole(existingMember.get().getRole());
+	  			  memberDto.setProfileUrl(existingMember.get().getProfileUrl());
+	  			  
+	  			 response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+	             response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+	  
+	             PrintWriter writer = response.getWriter();
+	             writer.println(memberDto);
+	             writer.flush();
+	  			 
             }
         } catch (Exception e) {
             throw e;
@@ -71,14 +97,5 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     }
 
-    // TODO : 소셜 로그인 시에도 무조건 토큰 생성하지 말고 JWT 인증 필터처럼 RefreshToken 유/무에 따라 다르게 처리해보기
-    private void loginSuccess(HttpServletResponse response, OAuth2User oAuth2User) throws IOException {
-//    		String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
-//        String refreshToken = jwtService.createRefreshToken();
-//        response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
-//        response.addHeader(jwtService.getRefreshHeader(), "Bearer " + refreshToken);
-//
-//        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
-//        jwtService.updateRefreshToken(oAuth2User.getEmail(), refreshToken);
-    }
+
 }
