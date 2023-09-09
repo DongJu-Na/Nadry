8e0cc6577164ddb3760e2c3b2fda58148ff9fe65
ndj <ehdeld123@naver.com>
2023-09-09 17:44:57 +0900

    style : 여행 리뷰 컬럼 제거

--- src/main/java/com/nadeul/ndj/entity/Dibs.java
+++ src/main/java/com/nadeul/ndj/entity/Dibs.java
@@ -28,10 +28,6 @@ public class Dibs {
   @JoinColumn(name = "memId", insertable = true, updatable = true)
   public Member member;
   
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "rvId", insertable = true, updatable = true)
-  public Review review;
-  
   private LocalDateTime dibsDate;
   
   private String contentId;
