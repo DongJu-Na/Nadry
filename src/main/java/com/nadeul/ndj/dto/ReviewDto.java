870edb966882d4950694a927772097d3d13c84b0
ndj <ehdeld123@naver.com>
2023-09-12 23:09:26 +0900

    style : reviewDto 수정

--- src/main/java/com/nadeul/ndj/dto/ReviewDto.java
+++ src/main/java/com/nadeul/ndj/dto/ReviewDto.java
@@ -1,10 +1,13 @@
 package com.nadeul.ndj.dto;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
 import com.nadeul.ndj.entity.Review;
+import com.nadeul.ndj.entity.ReviewGrade;
+import com.nadeul.ndj.entity.ReviewLike;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -13,6 +16,10 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 @Schema(description = "리뷰 DTO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ReviewDto {
 	
     @Data
@@ -28,15 +35,17 @@ public class ReviewDto {
     @Getter
     public static class ListResponse {
 	  @Schema(description = "여행 리뷰 목록" , example = "객체 반환")
-	  private List<Review> review;
+	  private List<ReviewDto> review;
 	  private BigDecimal averageRating;
 	  private int totalLikes;
 	  
-	  public ListResponse(List<Review> review, BigDecimal averageRating, int totalLikes) {
+	  public ListResponse(List<ReviewDto> review, BigDecimal averageRating, int totalLikes) {
 	        this.review = review;
 	        this.averageRating = averageRating;
 	        this.totalLikes = totalLikes;
 	  }
+	  
+	  
 
     }
     
@@ -66,5 +75,32 @@ public class ReviewDto {
     	@Schema(description = "리뷰 번호" , example = "1")
 	    private String rvId;
     }
+    
+    private Integer rvId;
+    private String contentId;
+    private String content;
+    private String reviewImageUrl;
+    private LocalDateTime createDate;
+    private String createBy;
+    private LocalDateTime updateDate;
+    private String updateBy;
+    private MemberDto member;
+    private List<ReviewGrade> reviewGrade;
+    private List<ReviewLike> reviewLike;
+    
+    public ReviewDto(Review review) {
+        this.rvId = review.getRvId();
+        this.contentId = review.getContentId();
+        this.content = review.getContent();
+        this.reviewImageUrl = review.getReviewImageUrl();
+        this.createDate = review.getCreateDate();
+        this.createBy = review.getCreateBy();
+        this.updateDate = review.getUpdateDate();
+        this.updateBy = review.getUpdateBy();
+        this.reviewGrade = review.getReviewGrade();
+        this.reviewLike = review.getReviewLike();
+        this.member = new MemberDto(review.getMember());
+      }
+
     
 }
