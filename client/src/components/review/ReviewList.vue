b4c29574c1b5f8f136fc1bb055c207f29ffb2b31
buzzrstudio <keepsoul@gmail.com>
2023-09-13 00:17:57 +0900

    edit: 리뷰 리스트 수정

--- client/src/components/review/ReviewList.vue
+++ client/src/components/review/ReviewList.vue
@@ -1,25 +1,63 @@
 <template>
-  <div v-for="review in reviews" :key="review.rvId" class="flex flex-col mt-5">
-    <div>{{ review.createBy }}</div>
-    <img v-if="review.reviewImageUrl" :src="`${storageUrl}${review.reviewImageUrl}`" alt="" />
-    <!-- <div>{{ review.reviewImageUrl }}</div> -->
-    <div>{{ review.content }}</div>
-    <div>{{ review.likes }}</div>
-    <!-- <div v-if="review.reviewGrade">{{ review.reviewGrade[0].rating }}</div> -->
-    <div>좋아요</div>
+  <div v-for="review in reviews" :key="review.rvId" class="flex flex-col pb-10 mt-10 border-b">
+    <!-- profile -->
+    <div class="flex items-center justify-between mb-5">
+      <!-- user profile image -->
+      <div
+        class="w-[40px] h-[40px] bg-zinc-100 mr-3 rounded-full flex justify-center itencen overflow-hidden"
+      >
+        <img v-if="review.member.profileUrl" :src="storageUrl + review.member.profileUrl" />
+        <UserIcon v-else class="w-5 opacity-10" />
+      </div>
+      <div class="flex flex-col leading-none">
+        <div class="mb-1">
+          <span class="mr-2 font-semibold">{{ review.member.name }}</span>
+          <span>{{ review.member.email }}</span>
+        </div>
+        <div class="text-xs text-zinc-400">{{ transDate(review.createDate) }}</div>
+      </div>
+      <div class="flex gap-5 ml-auto">
+        <!-- rating -->
+        <div v-if="review.reviewGrade" class="flex items-center gap-1">
+          <StarIcon class="w-4 text-yellow-400" />
+          <span>{{ review.reviewGrade[0].rating }}</span>
+        </div>
+        <!-- like -->
+        <div
+          @click="like(review.rvId)"
+          class="flex items-center gap-1 p-2 leading-none transition-all border rounded border-rose-300 text-rose-500 active:scale-95"
+        >
+          <HandThumbUpIcon class="w-4" />
+          <div>{{ review.like }}</div>
+        </div>
+      </div>
+    </div>
+    <!-- image -->
+    <img
+      v-if="review.reviewImageUrl"
+      :src="`${storageUrl}${review.reviewImageUrl}`"
+      class="mb-5 rounded-md"
+    />
+    <!-- content -->
+    <div class="p-5 text-sm rounded-md bg-zinc-50 text-zinc-500">{{ review.content }}</div>
   </div>
 </template>
 
 <script setup>
-import { ref, onBeforeMount } from 'vue';
+import { ref, onBeforeMount, computed } from 'vue';
 import { useRoute } from 'vue-router';
-import { getTripReview } from '@/api';
+import { getTripReview, postTripLike } from '@/api';
+import dayjs from 'dayjs';
+import { UserIcon, HandThumbUpIcon, StarIcon } from '@heroicons/vue/24/solid';
 
 const route = useRoute();
 const reviews = ref(null);
 
 const storageUrl = import.meta.env.VITE_APP_STORAGE_URL;
-// const storageUrl = 'http://218.236.144.129:8080';
+
+const transDate = (date) => {
+  return dayjs(date).format('YYYY년 MM월 DD일 HH:mm');
+};
 
 const fetchReviews = async () => {
   try {
@@ -38,6 +76,20 @@ const fetchReviews = async () => {
     if (review) {
       reviews.value = review;
     }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const like = async (rvId) => {
+  console.log(rvId);
+  const payload = {
+    rvId: rvId,
+    contentId: route.params.id,
+  };
+  try {
+    const result = await postTripLike(payload);
+    console.log(result);
   } catch (error) {
     console.log(error);
   }
