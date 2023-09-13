5e54ee2c7ef5eedcc4ad1eba3e6b08c9b70d6455
buzzrstudio <keepsoul@gmail.com>
2023-09-14 01:17:27 +0900

    edit: Circle Profile

--- client/src/components/common/moleclues/CircleProfile.vue
+++ client/src/components/common/moleclues/CircleProfile.vue
@@ -2,24 +2,30 @@
   <div class="relative">
     <!-- alarm -->
     <div
-      class="absolute top-[-5px] right-[-5px] flex justify-center items-center bg-emerald-500 w-5 h-5 rounded-full text-xs font-semibold text-white"
+      class="absolute top-[-3px] right-[-3px] flex justify-center items-center bg-rose-500 w-5 h-5 rounded-full text-xs font-semibold text-white"
     >
       {{ alarm }}
     </div>
     <!-- profile image -->
-    <img
-      ref="profileImage"
-      class="object-cover rounded-full aspect-square"
-      src=""
-      :width="props.size"
-    />
+    <div class="flex justify-center overflow-hidden rounded-full bg-zinc-100 w-[60px] h-[60px]">
+      <img
+        class="object-cover rounded-full aspect-square"
+        v-if="store.user.userInfo.profileUrl"
+        :src="storageUrl + store.user.userInfo.profileUrl"
+        :width="props.size"
+      />
+      <UserIcon v-else class="w-[30px] opacity-10" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 import { useMainStore } from '@/store';
+import { UserIcon } from '@heroicons/vue/20/solid';
+
 const store = useMainStore();
+const storageUrl = import.meta.env.VITE_APP_STORAGE_URL;
 
 const props = defineProps({
   alarm: {
--- client/src/components/home/Greetings.vue
+++ client/src/components/home/Greetings.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="flex justify-between items-center px-5 my-5">
+  <div class="flex items-center justify-between px-5 my-5">
     <h2 class="text-2xl font-medium">오늘, 나들이는<br />어디로 떠나볼까요?</h2>
-    <CircleProfile />
+    <CircleProfile v-if="store.user.isLoggedIn" />
   </div>
 </template>
 
 <script setup>
+import { useMainStore } from '@/store';
 import CircleProfile from '@/components/common/moleclues/CircleProfile.vue';
+
+const store = useMainStore();
 </script>
