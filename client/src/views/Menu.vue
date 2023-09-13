5275217d182e646291aae76daeed5f9473feec3d
buzzrstudio <keepsoul@gmail.com>
2023-09-14 01:18:04 +0900

    edit: Menu view

--- client/src/views/Menu.vue
+++ client/src/views/Menu.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="px-5">
-    <nav class="flex flex-col gap-3">
-      <router-link to="/login" class="p-3 bg-zinc-50">로그인</router-link>
-      <router-link to="/join" class="p-3 bg-zinc-50">회원가입</router-link>
-      <router-link to="/logout" class="p-3 bg-zinc-50">로그아웃</router-link>
+    <!-- user info -->
+    <div v-if="store.user.isLoggedIn" class="flex flex-col items-center justify-center mb-10">
+      <div
+        class="w-[80px] h-[80px] bg-zinc-100 rounded-full flex justify-center itencen overflow-hidden"
+      >
+        <img v-if="user.profileUrl" :src="storageUrl + user.profileUrl" />
+        <UserIcon v-else class="w-10 opacity-10" />
+      </div>
+      <div class="mt-3">{{ user.name }}</div>
+      <div class="text-sm text-zinc-400">{{ user.email }}</div>
+    </div>
+
+    <!-- menu -->
+    <nav class="menu">
+      <template v-if="!store.user.isLoggedIn">
+        <router-link to="/login">로그인</router-link>
+        <router-link to="/join">회원가입</router-link>
+      </template>
+      <router-link v-if="store.user.isLoggedIn" to="/logout">로그아웃</router-link>
     </nav>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from 'vue';
+import { useMainStore } from '@/store';
+import { UserIcon } from '@heroicons/vue/20/solid';
 
-<style lang="scss" scoped></style>
+const store = useMainStore();
+const storageUrl = import.meta.env.VITE_APP_STORAGE_URL;
+
+const user = computed(() => {
+  return store.user.userInfo;
+});
+</script>
