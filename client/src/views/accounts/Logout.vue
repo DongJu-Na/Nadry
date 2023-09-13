18ce62e370fac9de5e435fdfc15ebc5daaddcc72
buzzrstudio <keepsoul@gmail.com>
2023-09-13 23:18:52 +0900

    edit: 로그아웃 페이지

--- client/src/views/accounts/Logout.vue
+++ client/src/views/accounts/Logout.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>로그아웃</div>
+  <div class="flex flex-col items-center justify-center py-20">
+    <img src="/svg/page_logout.svg" class="w-2/3" />
+    <h1 class="text-lg text-zinc-400">안전하게 로그아웃 되었습니다.</h1>
+    <router-link to="/" class="mt-5 button">
+      <i class="mr-2 text-xl las la-home"></i>
+      <span>홈으로</span>
+    </router-link>
+  </div>
 </template>
 
 <script setup>
