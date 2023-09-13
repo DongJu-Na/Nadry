44ca7e22a434c4e566ceb1b57a8c04878e63b1b8
buzzrstudio <keepsoul@gmail.com>
2023-09-14 01:18:16 +0900

    add: Review 페이지

--- client/src/views/Review.vue
+++ client/src/views/Review.vue
@@ -1,35 +1,72 @@
 <template>
-  <div class="flex justify-center items-center text-zinc-300 h-[500px] text-xs">리뷰</div>
-  <h1>테스트중</h1>
+  <div class="px-5">
+    <!-- tab -->
+    <div class="">
+      <nav class="flex space-x-4" aria-label="Tabs">
+        <a
+          v-for="(tab, index) in tabs"
+          @click="clickTab(index)"
+          :key="tab.name"
+          :class="[
+            tab.current ? 'bg-green-100 text-green-700' : 'text-zinc-400',
+            'rounded-md px-3 py-2 text-sm font-medium transition-all',
+          ]"
+          >{{ tab.name }}</a
+        >
+      </nav>
+    </div>
+
+    <!-- my review -->
+    <div v-for="review in myReviews" :key="review.rvId" class="flex flex-col pb-10 mt-10 border-b">
+      {{ review }}
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import {
-  getMyTripReview,
-} from '@/api';
+import { getMyTripReview } from '@/api';
 
 const router = useRouter();
 
-const myReviewList = ref(null);
+// 1: 인기리뷰 / 2: 내가 작성한 리뷰
+const currentType = ref(1);
+const myReviews = ref(null);
+
+const tabs = ref([
+  {
+    name: '실시간 인기 리뷰',
+    current: true,
+  },
+  {
+    name: '내가 작성한 리뷰',
+    current: false,
+  },
+]);
+
+const clickTab = (index) => {
+  console.log(index);
+  tabs.value.forEach((tab) => {
+    tab.current = false;
+  });
+  tabs.value[index].current = true;
+};
 
 const fetchMyReviews = async () => {
   try {
     const payload = {
       page: 1,
       size: 10,
-      sort : ["createDate"],
+      sort: ['createDate'],
     };
     const {
       status,
-      data: {
-        data: { review },
-      },
+      data: { data },
     } = await getMyTripReview(payload);
-    console.log('MyTripReview: ', review);
-    if (review) {
-      reviews.value = review;
+    // console.log('MyTripReview: ', data);
+    if (status === 200 && data) {
+      myReviews.value = data;
     }
   } catch (error) {
     console.log(error);
