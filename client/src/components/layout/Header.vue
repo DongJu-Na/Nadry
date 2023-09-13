8952327a5f31c611199290c12591bbe2fb273455
buzzrstudio <keepsoul@gmail.com>
2023-09-14 01:39:15 +0900

    edit: header

--- client/src/components/layout/Header.vue
+++ client/src/components/layout/Header.vue
@@ -3,10 +3,12 @@
     class="fixed top-0 left-0 right-0 mx-auto bg-white flex justify-between items-center w-full h-[60px] z-[100]"
     :style="`width:var(--app-width)`"
   >
+    <!-- logo -->
     <router-link to="/">
-      <img src="/svg/logo.svg" alt="Nadry" class="w-[90px] pl-5" />
+      <img src="/svg/logo.svg" alt="Nadry" class="w-[110px] h-[60px] px-5 py-3" />
     </router-link>
-    <!-- <h1 class="pl-5 text-2xl font-semibold text-emerald-500">Nadry</h1> -->
+
+    <!-- menu -->
     <router-link to="/menu" class="block p-[18px]">
       <IconMenu />
     </router-link>
@@ -15,8 +17,4 @@
 
 <script setup>
 import IconMenu from '@/components/common/icons/IconMenu.vue';
-
-const openMenu = () => {
-  // console.log('open menu');
-};
 </script>
