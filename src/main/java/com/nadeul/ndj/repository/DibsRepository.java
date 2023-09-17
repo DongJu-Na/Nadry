08f37e3e7546c28ed09a6315768acd7b6d3f5f22
ndj <ehdeld123@naver.com>
2023-09-17 21:53:21 +0900

    refact : list , dibs 기능 수정

--- src/main/java/com/nadeul/ndj/repository/DibsRepository.java
+++ src/main/java/com/nadeul/ndj/repository/DibsRepository.java
@@ -1,6 +1,7 @@
 package com.nadeul.ndj.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +11,7 @@ import com.nadeul.ndj.entity.Member;
 
 public interface DibsRepository extends JpaRepository<Dibs, Integer> {
 	
-    boolean findByMemberAndContentId(Member member,String contentId);
+	Optional<Dibs> findByMemberAndContentId(Member member,String contentId);
     
     List<Dibs> findByMemberOrderByDibsDateDesc(Member member , Pageable pageable);
     
