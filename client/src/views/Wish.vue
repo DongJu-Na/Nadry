f04fe28beca5034205ae7ddf7fdb66932564c2db
buzzrstudio <keepsoul@gmail.com>
2023-09-23 01:49:55 +0900

    edit: wish page fetch

--- client/src/views/Wish.vue
+++ client/src/views/Wish.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="flex justify-center items-center text-zinc-300 h-[500px] text-xs">찜</div>
+  <div class="flex justify-center items-center text-zinc-300 h-[500px] text-xs">
+    <div></div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue';
+import { getWishList } from '@/api';
 
-<style lang="scss" scoped></style>
+const fetchWishList = async () => {
+  try {
+    const result = await getWishList();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+onMounted(() => {
+  fetchWishList();
+});
+</script>
