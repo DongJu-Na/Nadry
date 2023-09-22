2467c0fbabefdb10d37a60f4d2025d0229bfe0dd
buzzrstudio <keepsoul@gmail.com>
2023-09-23 01:49:00 +0900

    edit: 여행 상세 > 찜 추가

--- client/src/views/trip/Detail.vue
+++ client/src/views/trip/Detail.vue
@@ -24,30 +24,21 @@
 
     <!-- infomation -->
     <div class="px-5 mt-10">
-      <!-- extra -->
-      <!-- <div class="flex">
-        <div>
-          <img src="" alt="" />
-          <span>찜하기</span>
-        </div>
-        <div>
-          <img src="" alt="" />
-          <span>공유하기</span>
-        </div>
-      </div> -->
-      <!-- tag -->
-      <!-- <div class="flex text-emerald-500">
-        <span>#경주</span>
-        <span>#유적지</span>
-        <span>#핫플</span>
-      </div> -->
       <!-- title -->
-      <h1 class="text-2xl">{{ detailCommon.title }}</h1>
-      <div class="flex gap-1 mt-2 text-sm text-zinc-400">
-        <p>{{ detailCommon.addr1 }}</p>
-        <p>{{ detailCommon.addr2 }}</p>
+      <div class="flex justify-between">
+        <div>
+          <h1 class="text-2xl">{{ detailCommon.title }}</h1>
+          <div class="flex gap-1 mt-2 text-sm text-zinc-400">
+            <p>{{ detailCommon.addr1 }}</p>
+            <p>{{ detailCommon.addr2 }}</p>
+          </div>
+          <div v-html="detailCommon.homepage" class="mt-1 text-sm text-blue-600"></div>
+        </div>
+        <div @click="clickWish" class="flex flex-col items-center gap-[3px] text-rose-600">
+          <HeartOutlineIcon class="w-[25px]" />
+          <span class="text-xs leading-none">찜하기</span>
+        </div>
       </div>
-      <div v-html="detailCommon.homepage" class="mt-1 text-sm text-blue-600"></div>
       <!-- rating & reviews -->
       <div class="flex gap-3 mt-3 text-xs">
         <div class="flex items-center gap-1">
@@ -105,6 +96,7 @@ import {
   getTripDetailInfo,
   getTripDetailImage,
   getTripDetailCommon,
+  addWish,
 } from '@/api';
 import { useMainStore } from '@/store';
 import { Pagination } from 'swiper';
@@ -113,6 +105,8 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import ReviewModal from '@/components/modal/ReviewModal.vue';
 import ReviewList from '@/components/review/ReviewList.vue';
+import { HeartIcon as HeartSolidIcon } from '@heroicons/vue/24/solid';
+import { HeartIcon as HeartOutlineIcon } from '@heroicons/vue/24/outline';
 
 const modules = [Pagination];
 
@@ -127,6 +121,28 @@ const detailCommon = ref(null);
 const imageLoading = ref(false);
 const reviews = ref([]);
 const showReviewModal = ref(false);
+
+// 찜하기
+const clickWish = async () => {
+  console.log(detailCommon.value.title, detailCommon.value.firstimage);
+  const payload = {
+    contentId: route.params.id,
+    contentName: detailCommon.value.title,
+    contentImageUrl: detailCommon.value.firstimage,
+  };
+
+  try {
+    const {
+      status,
+      data: { resultMsg },
+    } = await addWish(payload);
+    if (status === 200 && resultMsg) {
+      alert(resultMsg);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // review
 const writeReview = () => {
