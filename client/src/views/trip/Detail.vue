d5d21ee2f8ba2a325d18cc2024603e7eb65f5ece
buzzrstudio <keepsoul@gmail.com>
2023-09-24 01:15:32 +0900

    edit: 여행 상세 > 이용안내 정리

--- client/src/views/trip/Detail.vue
+++ client/src/views/trip/Detail.vue
@@ -25,16 +25,19 @@
     <!-- infomation -->
     <div class="px-5 mt-10">
       <!-- title -->
-      <div class="flex justify-between">
+      <div class="relative flex justify-between">
         <div>
           <h1 class="text-2xl">{{ detailCommon.title }}</h1>
-          <div class="flex gap-1 mt-2 text-sm text-zinc-400">
+          <div class="flex flex-col mt-2 mb-2 text-sm text-zinc-400">
             <p>{{ detailCommon.addr1 }}</p>
             <p>{{ detailCommon.addr2 }}</p>
           </div>
           <div v-html="detailCommon.homepage" class="mt-1 text-sm text-blue-600"></div>
         </div>
-        <div @click="clickWish" class="flex flex-col items-center gap-[3px] text-rose-600">
+        <div
+          @click="clickWish"
+          class="absolute top-0 right-0 flex flex-col items-center gap-[3px] text-rose-600"
+        >
           <HeartOutlineIcon class="w-[25px]" />
           <span class="text-xs leading-none">찜하기</span>
         </div>
@@ -67,7 +70,18 @@
       <!-- usage -->
       <div class="mt-10">
         <h3 class="mb-3 font-medium">이용안내</h3>
-        <div class="mt-3 text-xs text-zinc-400">{{ detailIntro }}</div>
+        <table class="flow-root min-w-full overflow-hidden border-t border-t-gray-400">
+          <tbody class="divide-y divide-gray-200/70">
+            <tr v-for="info in detailIntro" :key="info">
+              <td class="py-3 text-xs font-medium text-gray-500 whitespace-nowrap">
+                {{ info[0] }}
+              </td>
+              <td class="w-full py-3 pl-10 text-xs font-medium text-gray-800">
+                <p v-html="info[1]"></p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
       <!-- reviews -->
@@ -107,6 +121,7 @@ import ReviewModal from '@/components/modal/ReviewModal.vue';
 import ReviewList from '@/components/review/ReviewList.vue';
 import { HeartIcon as HeartSolidIcon } from '@heroicons/vue/24/solid';
 import { HeartIcon as HeartOutlineIcon } from '@heroicons/vue/24/outline';
+import { usageKeys } from '@/uilts/usageKeys.js';
 
 const modules = [Pagination];
 
@@ -144,6 +159,12 @@ const clickWish = async () => {
   }
 };
 
+// 이용안내 변환
+// const usageInfo = Array.from(detailIntro.value).forEach((item) => {
+//   console.log(item);
+//   return [];
+// });
+
 // review
 const writeReview = () => {
   // alert('리뷰 작성');
@@ -170,9 +191,26 @@ const fetchTripData = async () => {
         },
       },
     } = await getTripDetailIntro(payload);
-    console.log('detailIntro: ', status, item[0]);
+    // console.log('detailIntro: ', status, item[0]);
+
     if (status === 200 && item[0]) {
-      detailIntro.value = item[0];
+      let transIntro = [];
+      Object.keys(item[0]).filter((key) => {
+        if (
+          key !== 'contentid' &&
+          key !== 'contenttypeid' &&
+          item[0][key] &&
+          item[0][key] !== '0'
+        ) {
+          let matchedKey;
+          Object.keys(usageKeys).forEach((usageKey) => {
+            if (usageKey === key) matchedKey = usageKeys[usageKey];
+          });
+          transIntro.push([matchedKey, item[0][key]]);
+          // transIntro[matchedKey] = item[0][key];
+        }
+      });
+      detailIntro.value = transIntro;
     }
   } catch (error) {
     console.log(error);
@@ -194,8 +232,8 @@ const fetchTripData = async () => {
         },
       },
     } = await getTripDetailInfo(payload);
-    console.log('detailInfo: ', status, item[0]);
-    if (status === 200 && item[0]) {
+    console.log('detailInfo: ', status, item);
+    if (status === 200 && item && item[0]) {
       detailInfo.value = item[0];
     }
   } catch (error) {
