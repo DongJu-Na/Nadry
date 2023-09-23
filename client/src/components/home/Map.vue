47c4e09069f3c890ace9dd485df4133232019ccb
ndj <ehdeld123@naver.com>
2023-09-24 04:13:45 +0900

    feat : 카카오맵  기능 추가

--- client/src/components/home/Map.vue
+++ client/src/components/home/Map.vue
@@ -6,17 +6,160 @@
       <div class="absolute z-[99] p-3 w-full">
         <div class="flex items-center w-full p-3 bg-white rounded-md shadow-md">
           <i class="mr-2 text-xl las la-search text-zinc-400"></i>
-          <input type="text" placeholder="지금, 경주를 검색해보세요!" />
+            <input  
+                v-model="searchWord" 
+                @keyup.enter="onEnter"
+                type="text" 
+                placeholder="지금, 경주를 검색해보세요!" 
+            />
         </div>
       </div>
     </div>
   </div>
 </template>
 
+<style>
+    .wrap {position: absolute;left: 0;bottom: 40px;width: 288px;height: 132px;margin-left: -144px;text-align: left;overflow: hidden;font-size: 12px;font-family: 'Malgun Gothic', dotum, '돋움', sans-serif;line-height: 1.5;}
+    .wrap * {padding: 0;margin: 0;}
+    .wrap .info {width: 286px;height: 120px;border-radius: 5px;border-bottom: 2px solid #ccc;border-right: 1px solid #ccc;overflow: hidden;background: #fff;}
+    .wrap .info:nth-child(1) {border: 0;box-shadow: 0px 1px 2px #888;}
+    .info .title {padding: 5px 0 0 10px;height: 30px;background: #eee;border-bottom: 1px solid #ddd;font-size: 18px;font-weight: bold;}
+    .info .close {position: absolute;top: 10px;right: 10px;color: #888;width: 17px;height: 17px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png');}
+    .info .close:hover {cursor: pointer;}
+    .info .body {position: relative;overflow: hidden;}
+    .info .desc {position: relative;margin: 13px 0 0 90px;height: 75px;}
+    .desc .ellipsis {overflow: hidden;text-overflow: ellipsis;white-space: nowrap;}
+    .desc .jibun {font-size: 11px;color: #888;margin-top: -2px;}
+    .info .img {position: absolute;top: 6px;left: 5px;width: 73px;height: 71px;border: 1px solid #ddd;color: #888;overflow: hidden;}
+    .info:after {content: '';position: absolute;margin-left: -12px;left: 50%;bottom: 0;width: 22px;height: 12px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
+    .info .link {color: #5085BB;}
+</style>
+
 <script setup>
 import { onMounted, ref } from 'vue';
+import { searchTrip } from '@/api';
+
+const searchWord = ref('');
 
 let map;
+let displayMarkers = [];
+
+const submit = async () => {
+  if (!searchWord.value) {
+    alert('검색어를 입력하세요.');
+    return;
+  }
+
+  try {
+    const payload = {
+      numOfRows: 30,
+      pageNo: 1,
+      MobileOS: 'ETC',
+      MobileApp: 'AppTest',
+      _type: 'json',
+      listYN: 'Y',
+      contentTypeId: '',
+      arrange: 'A',
+      areaCode: '',
+      sigunguCode: '',
+      cat1: '',
+      cat2: '',
+      cat3: '',
+      keyword: searchWord.value,
+    };
+
+    const {
+      status,
+      data: {
+        response: { body },
+      },
+    } = await searchTrip(payload);
+        console.log(displayMarkers);
+        for (var i = 0; i < displayMarkers.length; i++) {
+            displayMarkers[i].setMap(null);
+        }    
+      
+    if (status === 200 && body && body.items.item.length > 0) {
+        body.items.item.forEach(item => {
+          map.relayout();
+          let kakaoPos = new kakao.maps.LatLng(item.mapy, item.mapx);
+          let marker = new kakao.maps.Marker({
+            map: map,
+            position: kakaoPos,
+            title: item.title,
+            clickable: true
+          });
+
+          console.log("위치 잘 찍히니? 네 => " + marker.getTitle());
+
+          let imageSrc = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
+          let imageSize = new kakao.maps.Size(37, 45);
+          let imageOption = { offset: new kakao.maps.Point(17, 40) };
+
+          let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+          let markerPosition = marker.getPosition();
+
+          marker = new kakao.maps.Marker({
+            position: markerPosition,
+            image: markerImage
+          });
+
+          marker.setMap(map);
+          displayMarkers.push(marker);
+          let imgUrl = (item.firstimage === null || item.firstimage === ""  ? "/svg/empty_face.svg" : item.firstimage);
+
+          let contentHtml =  `<div class="wrap">
+                                <div class="info">
+                                  <div class="title">
+                                    ${item.title}
+                                  </div>
+                                  <div class="body">
+                                    <div class="img">
+                                      <img src=${imgUrl} width="73" height="70">
+                                    </div>
+                                    <div class="desc">
+                                      <div class="ellipsis"></div>
+                                      <div class="jibun ellipsis">${item.addr1 + " " + item.addr2}</div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            `;
+          
+            let overlay = new kakao.maps.CustomOverlay({
+                content: contentHtml,
+                map: map,
+                position: marker.getPosition()       
+            });
+
+            overlay.setMap(null);  
+
+            kakao.maps.event.addListener(marker, 'click', function() {
+                window.location = `${"/trips/" + item.contentid + "/" +item.contenttypeid}`;
+            });
+            
+            kakao.maps.event.addListener(marker, "mouseover", () => {
+              // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
+              overlay.setMap(map);
+            });
+
+            kakao.maps.event.addListener(marker, "mouseout", () => {
+               overlay.setMap(null);   
+            });
+
+                
+        });
+    }else{
+      alert("검색 결과가 없습니다.");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const onEnter = () => {
+  submit(searchWord.value);
+};
 
 const getLocation = () => {
   if (navigator.geolocation) {
