5104ca2ca53ccc9a7956e0e59c1d41e98c51c092
buzzrstudio <keepsoul@gmail.com>
2023-09-24 23:18:32 +0900

    edit: home > map

--- client/src/components/home/Map.vue
+++ client/src/components/home/Map.vue
@@ -1,48 +1,78 @@
 <template>
-  <div class="flex flex-col my-10">
+  <div class="flex flex-col mt-10">
     <h2 class="px-5 mb-3 text-lg font-semibold">주변을 둘러 보세요</h2>
     <div id="map" class="w-full h-[500px]">
       <!-- input -->
       <div class="absolute z-[99] p-3 w-full">
-        <div class="flex items-center w-full p-3 bg-white rounded-md shadow-md">
-          <i class="mr-2 text-xl las la-search text-zinc-400"></i>
-            <input  
-                v-model="searchWord" 
-                @keyup.enter="onEnter"
-                type="text" 
-                placeholder="지금, 경주를 검색해보세요!" 
-            />
+        <div class="flex items-center w-full p-1 pl-3 bg-white rounded-md shadow-md">
+          <i class="text-xl las la-search text-zinc-400"></i>
+          <input
+            v-model="searchWord"
+            @keyup.enter="submit(searchWord)"
+            type="text"
+            placeholder="지금, 경주를 검색해보세요!"
+            class="w-full p-2"
+          />
         </div>
       </div>
+    </div>
+    <!-- items -->
+    <div class="flex z-[100] -translate-y-10">
+      <Swiper
+        :slides-per-view="2.5"
+        :space-between="10"
+        :slides-offset-before="10"
+        :slides-offset-after="20"
+        :loop="false"
+      >
+        <SwiperSlide v-for="(item, index) in searchResults" :key="item.id">
+          <div
+            @click="onCenterForClick(index)"
+            class="overflow-hidden bg-white rounded-md shadow-md"
+          >
+            <img
+              v-if="item.firstimage"
+              :src="item.firstimage"
+              class="object-cover w-full h-[100px]"
+            />
+            <div v-else class="flex items-center justify-center w-full h-[100px] bg-zinc-50">
+              <PhotoIcon class="w-8 text-zinc-200" />
+            </div>
+            <div class="p-3">
+              <div class="text-xs font-medium truncate ...">{{ item.title }}</div>
+              <div class="text-2xs text-zinc-400 truncate ...">{{ item.addr1 }}</div>
+              <router-link
+                :to="`/trips/${item.contentid}/${item.contenttypeid}`"
+                class="flex justify-center w-full px-2 py-2 mt-2 text-xs font-semibold text-gray-900 bg-white rounded shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              >
+                자세히보기
+              </router-link>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   </div>
 </template>
 
-<style>
-    .wrap {position: absolute;left: 0;bottom: 40px;width: 288px;height: 132px;margin-left: -144px;text-align: left;overflow: hidden;font-size: 12px;font-family: 'Malgun Gothic', dotum, '돋움', sans-serif;line-height: 1.5;}
-    .wrap * {padding: 0;margin: 0;}
-    .wrap .info {width: 286px;height: 120px;border-radius: 5px;border-bottom: 2px solid #ccc;border-right: 1px solid #ccc;overflow: hidden;background: #fff;}
-    .wrap .info:nth-child(1) {border: 0;box-shadow: 0px 1px 2px #888;}
-    .info .title {padding: 5px 0 0 10px;height: 30px;background: #eee;border-bottom: 1px solid #ddd;font-size: 18px;font-weight: bold;}
-    .info .close {position: absolute;top: 10px;right: 10px;color: #888;width: 17px;height: 17px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/overlay_close.png');}
-    .info .close:hover {cursor: pointer;}
-    .info .body {position: relative;overflow: hidden;}
-    .info .desc {position: relative;margin: 13px 0 0 90px;height: 75px;}
-    .desc .ellipsis {overflow: hidden;text-overflow: ellipsis;white-space: nowrap;}
-    .desc .jibun {font-size: 11px;color: #888;margin-top: -2px;}
-    .info .img {position: absolute;top: 6px;left: 5px;width: 73px;height: 71px;border: 1px solid #ddd;color: #888;overflow: hidden;}
-    .info:after {content: '';position: absolute;margin-left: -12px;left: 50%;bottom: 0;width: 22px;height: 12px;background: url('https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png')}
-    .info .link {color: #5085BB;}
-</style>
-
 <script setup>
 import { onMounted, ref } from 'vue';
 import { searchTrip } from '@/api';
-
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import { PhotoIcon } from '@heroicons/vue/24/solid';
 
 const searchWord = ref('');
+const searchResults = ref([]);
+const markers = ref([]);
 
-let map,displayMarkers = [];
+let map;
+
+const onCenterForClick = (index) => {
+  map.setCenter(
+    new kakao.maps.LatLng(searchResults.value[index].mapy, searchResults.value[index].mapx)
+  );
+};
 
 const submit = async () => {
   if (!searchWord.value) {
@@ -50,9 +80,18 @@ const submit = async () => {
     return;
   }
 
+  if (searchResults.value.length > 0) {
+    searchResults.value = [];
+    markers.value.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers.value = [];
+    console.log(searchResults.value, markers.value);
+  }
+
   try {
     const payload = {
-      numOfRows: 10,
+      numOfRows: 30,
       pageNo: 1,
       MobileOS: 'ETC',
       MobileApp: 'AppTest',
@@ -68,111 +107,38 @@ const submit = async () => {
       keyword: searchWord.value,
     };
 
-     await searchTrip(payload).then((result)=>{
-    const {status, data: {response: { body }}} = result;
-      console.log(displayMarkers);
+    await searchTrip(payload).then((result) => {
+      const {
+        status,
+        data: {
+          response: { body },
+        },
+      } = result;
 
-        if (status === 200 && body && body.items.item.length > 0) {
-            map.relayout();
-            hideMarkers();
-            body.items.item.forEach(item => {
-              let kakaoPos = new kakao.maps.LatLng(item.mapy, item.mapx);
+      if (status === 200 && body && body.items.item.length > 0) {
+        body.items.item.forEach((item) => {
+          searchResults.value.push(item);
+          let kakaoMapPosition = new kakao.maps.LatLng(item.mapy, item.mapx);
+          let marker = new kakao.maps.Marker({
+            position: new kakao.maps.LatLng(item.mapy, item.mapx),
+          });
+          marker.setMap(map);
+          markers.value.push(marker);
+        });
 
-              addMarker(kakaoPos,item,map);
-
-            });
-
-            let firstSearchMapPos = new kakao.maps.LatLng(body.items.item[0].mapy , body.items.item[0].mapx);
-            map.setCenter(firstSearchMapPos);
-            showMarkers(map);
-
-        }else{
-          alert("검색 결과가 없습니다.");
-        }
+        let firstSearchMapPos = new kakao.maps.LatLng(
+          body.items.item[0].mapy,
+          body.items.item[0].mapx
+        );
+        map.setCenter(firstSearchMapPos);
+      } else {
+        alert('검색 결과가 없습니다.');
+      }
     });
-        
   } catch (error) {
     console.log(error);
   }
 };
-
-function addMarker(_position,_item,_map) {
-    let marker = new kakao.maps.Marker({
-      map: _map,
-      position: _position,
-      title: _item.title,
-      clickable: true
-    });
-
-    let imageSrc = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-    let imageSize = new kakao.maps.Size(37, 45);
-    let imageOption = { offset: new kakao.maps.Point(17, 40) };
-
-    let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-    let markerPosition = marker.getPosition();
-
-    marker = new kakao.maps.Marker({
-      position: markerPosition,
-      // image: markerImage
-    });
-
-    marker.setMap(_map);
-    let imgUrl = (_item.firstimage === null || _item.firstimage === ""  ? "/svg/empty_face.svg" : _item.firstimage);
-
-    let contentHtml =  `<div class="wrap">
-                          <div class="info">
-                            <div class="title">
-                              ${_item.title}
-                              <div class="close" onclick="closeOverlay()" title="닫기"></div>
-                            </div>
-                            <div class="body">
-                              <div class="img">
-                                <img src=${imgUrl} width="73" height="70">
-                              </div>
-                              <div class="desc">
-                                <div class="ellipsis"></div>
-                                <div class="jibun ellipsis">${_item.addr1 + " " + _item.addr2}</div>
-                                <div><a href=${"/trips/" + _item.contentid + "/" +_item.contenttypeid} class="link">상세보기</a></div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      `;
-    
-      let overlay = new kakao.maps.CustomOverlay({
-          content: contentHtml,
-          map: _map,
-          position: marker.getPosition()       
-      });
-
-
-      kakao.maps.event.addListener(marker, 'click', function() {
-          overlay.setMap(_map);
-      });
-      
-    displayMarkers.push(marker);
-    console.debug("push Markers",displayMarkers);
-}
-
-function showMarkers(_map) {
-    setMarkers(_map);
-}
-
-function hideMarkers() {
-    setMarkers(null);    
-}
-
-function setMarkers(_map) {
-    for (var i = 0; i < displayMarkers.length; i++) {
-        displayMarkers[i].setMap(_map);
-    }
-    console.debug("setMarkers",displayMarkers,_map);            
-}
-
-function closeOverlay() {
-    console.debug("closeOverlay");
-    overlay.setMap(null);     
-}
 
 const onEnter = () => {
   submit(searchWord.value);
@@ -184,10 +150,11 @@ const getLocation = () => {
       const myPosition = new kakao.maps.LatLng(position.coords.latitude, position.coords.longitude);
       map.setCenter(myPosition);
 
-      const marker = new kakao.maps.Marker({
-        position: myPosition,
-        map: map,
-      });
+      // 내위치는 마커로 표시 하지 않아도 될듯
+      // const marker = new kakao.maps.Marker({
+      //   position: myPosition,
+      //   map: map,
+      // });
     });
   } else {
     console.log('getLocation error');
@@ -209,15 +176,15 @@ onMounted(() => {
   if (window.kakao && window.kakao.maps) {
     loadMap();
   } else {
-    
     const script = document.createElement('script');
-    script.src ='//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=' + import.meta.env.VITE_APP_KAKAO_KEY;
+    script.src =
+      '//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=' + import.meta.env.VITE_APP_KAKAO_KEY;
     script.onload = () => {
-      kakao.maps.load(() => {      
+      kakao.maps.load(() => {
         kakao.maps.load(loadMap);
       });
-    }
-    
+    };
+
     document.head.appendChild(script);
   }
 });
