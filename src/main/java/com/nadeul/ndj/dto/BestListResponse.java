20b4be41dd1288e9e35997f86da0b74bafd5314c
ndj <ehdeld123@naver.com>
2023-09-28 23:50:59 +0900

    style : 베스트리뷰 dto 수정

--- src/main/java/com/nadeul/ndj/dto/BestListResponse.java
+++ src/main/java/com/nadeul/ndj/dto/BestListResponse.java
@@ -17,9 +17,15 @@ public class BestListResponse {
   private LocalDateTime updateDate;
   private String updateBy;
   private Object likes;
+  private Object rating;
+  private Integer memId;
+  private String name;
+  private String email;
+  private String profileUrl;
   
   public BestListResponse(Integer rvId, String contentId, String content, String reviewImageUrl,
-          LocalDateTime createDate, String createBy, LocalDateTime updateDate, String updateBy, Object likes) {
+          LocalDateTime createDate, String createBy, LocalDateTime updateDate, String updateBy, Object likes
+          ,Object rating,Integer memId,String name,String email,String profileUrl) {
 	    this.rvId = rvId;
         this.contentId = contentId;
         this.content = content;
@@ -29,5 +35,10 @@ public class BestListResponse {
         this.updateDate = updateDate;
         this.updateBy = updateBy;
         this.likes = likes;
+        this.rating = rating;
+        this.memId = memId;
+        this.name = name;
+        this.email = email;
+        this.profileUrl = profileUrl;
   }
 } 
