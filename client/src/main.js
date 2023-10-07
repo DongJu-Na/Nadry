670b183bfafc61303f004c490c5601a088b033df
ndj <ehdeld123@naver.com>
2023-10-08 04:07:58 +0900

    chore : i8n 다국어 처리 라이브러리 추가 및 버전 업데이트

--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,8 @@ import { router } from '@/routes';
 import App from './App.vue';
 import Vue3Lottie from 'vue3-lottie';
 import './style.css';
+import { i18n } from "./i18n/i18n" 
 
 pinia.use(piniaPersist);
 
-createApp(App).use(pinia).use(router).use(Vue3Lottie).mount('#app');
+createApp(App).use(pinia).use(router).use(Vue3Lottie).use(i18n).mount('#app');
