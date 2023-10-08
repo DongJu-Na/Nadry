efced0e4c687fde33ec4148caed597b297bbffe1
ndj <ehdeld123@naver.com>
2023-10-09 03:10:44 +0900

    style : 불필요 import 제거

--- src/main/java/com/nadeul/ndj/entity/StampMaster.java
+++ src/main/java/com/nadeul/ndj/entity/StampMaster.java
@@ -1,13 +1,10 @@
 package com.nadeul.ndj.entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/com/nadeul/ndj/repository/StampRepository.java
+++ src/main/java/com/nadeul/ndj/repository/StampRepository.java
@@ -3,10 +3,7 @@ package com.nadeul.ndj.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import com.nadeul.ndj.dto.StampDto;
 import com.nadeul.ndj.entity.Stamp;
 
 public interface StampRepository extends JpaRepository<Stamp, Integer> {
