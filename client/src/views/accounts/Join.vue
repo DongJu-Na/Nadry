4ea6523f7d4daea1863877e7d33eb49c0056ee72
ndj <ehdeld123@naver.com>
2023-10-09 23:29:58 +0900

    style : 회원가입 페이지 다국어 처리

--- client/src/views/accounts/Join.vue
+++ client/src/views/accounts/Join.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="px-5 join">
-    <h2 class="text-xl font-medium">회원 가입</h2>
-    <p class="mb-5 text-sm text-zinc-400">아래 정보들을 입력해주세요</p>
+    <h2 class="text-xl font-medium">{{ $t('joinLabel1') }}</h2>
+    <p class="mb-5 text-sm text-zinc-400">{{ $t('joinLabel2') }}</p>
     <form @submit.prevent="submit">
       <!-- input -->
       <div v-for="input in inputData" :id="input.id" class="mb-5">
-        <label class="block mb-1 text-sm text-zinc-600">{{ input.title }}</label>
+        <label class="block mb-1 text-sm text-zinc-600">{{ $t(input.labelLang) }}</label>
         <input
           v-model="input.model.value"
           :type="input.type"
           class="input"
-          :placeholder="input.placeholder"
+          :placeholder="$t(input.placeholder)"
           autocomplete="0"
         />
       </div>
       <!-- profile image -->
       <div>
-        <label class="block mb-1 text-sm text-zinc-600">프로필 이미지</label>
+        <label class="block mb-1 text-sm text-zinc-600">{{$t('joinLabel8')}}</label>
         <div class="flex items-center gap-3">
           <div
             class="flex justify-center overflow-hidden rounded-full bg-zinc-100 w-[50px] h-[50px]"
@@ -39,12 +39,12 @@
             class="flex items-center gap-1.5 px-4 py-2 text-xs border rounded text-zinc-500"
           >
             <PhotoIcon class="w-4" />
-            <span>파일 업로드</span>
+            <span>{{$t('joinFileUploadTxt')}} </span>
           </div>
         </div>
       </div>
       <!-- button -->
-      <button class="mt-5">회원 가입</button>
+      <button class="mt-5">{{$t('joinBtnTxt')}}</button>
     </form>
   </div>
 </template>
@@ -70,36 +70,41 @@ const inputData = [
     id: 1,
     title: '이름',
     type: 'text',
-    placeholder: '이름을 입력하세요',
+    placeholder: 'joinPlaceholder1',
     model: name,
+    labelLang : "joinLabel3",
   },
   {
     id: 2,
     title: '이메일',
     type: 'email',
-    placeholder: '이메일을 입력하세요',
+    placeholder: 'joinPlaceholder2',
     model: email,
+    labelLang : "joinLabel4",
   },
   {
     id: 3,
     title: '비밀번호',
     type: 'password',
-    placeholder: '비밀번호를 입력하세요',
+    placeholder: 'joinPlaceholder3',
     model: password,
+    labelLang : "joinLabel5",
   },
   {
     id: 4,
     title: '비밀번호 확인',
     type: 'password',
-    placeholder: '동일한 비밀번호를 입력하세요',
+    placeholder: 'joinPlaceholder4',
     model: passwordConfirm,
+    labelLang : "joinLabel6",
   },
   {
     id: 5,
     title: '생년월일',
     type: 'date',
-    placeholder: '생년월일을 입력하세요',
+    placeholder: 'joinPlaceholder5',
     model: birthDay,
+    labelLang : "joinLabel7",
   },
 ];
 
